68ac8e8410cd24158899eaeda07cd316ddf065c1
zourui <zourui@mrray.cn>
2018-03-24 12:29:36 +0800

    新增面包屑

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
     <div id="app">
-        <header class="header"></header>
+        <header class="header clear">
+            <div class="fl logo"></div>
+            <div class="fl breadcrumb">
+                <el-breadcrumb separator-class="el-icon-arrow-right">
+                    <el-breadcrumb-item v-for="i in breadcrumbList" :key="i.id">{{i.name}}</el-breadcrumb-item>
+                </el-breadcrumb>
+            </div>
+        </header>
         <section class="section">
             <aside class="aside">
                 <el-menu default-active="1">
@@ -16,7 +23,96 @@
 
 <script>
     export default {
-        name: 'App'
+        name: 'App',
+        computed: {
+            breadcrumbList() {
+                let arr;
+                switch (this.$route.name){
+                    case 'projectList' :
+                        arr = [
+                            {
+                                id: '1',
+                                name: '接口管理',
+                                path: ''
+                            },
+                            {
+                                id: '2',
+                                name: '项目列表',
+                                path: ''
+                            }
+                        ];
+                        break;
+                    case 'apiList' :
+                        arr = [
+                            {
+                                id: '1',
+                                name: '接口管理',
+                                path: ''
+                            },
+                            {
+                                id: '2',
+                                name: '项目列表',
+                                path: ''
+                            },
+                            {
+                                id: '3',
+                                name: 'Api列表',
+                                path: ''
+                            }
+                        ];
+                        break;
+                    case 'addApi' :
+                        arr = [
+                            {
+                                id: '1',
+                                name: '接口管理',
+                                path: ''
+                            },
+                            {
+                                id: '2',
+                                name: '项目列表',
+                                path: ''
+                            },
+                            {
+                                id: '3',
+                                name: 'Api列表',
+                                path: ''
+                            },
+                            {
+                                id: '4',
+                                name: '添加Api',
+                                path: ''
+                            }
+                        ];
+                        break;
+                    case 'setApi' :
+                        arr = [
+                            {
+                                id: '1',
+                                name: '接口管理',
+                                path: ''
+                            },
+                            {
+                                id: '2',
+                                name: '项目列表',
+                                path: ''
+                            },
+                            {
+                                id: '3',
+                                name: 'Api列表',
+                                path: ''
+                            },
+                            {
+                                id: '4',
+                                name: '修改Api',
+                                path: ''
+                            }
+                        ];
+                        break;
+                }
+                return arr;
+            }
+        }
     }
 </script>
 
@@ -29,6 +125,21 @@
             height: 50px;
             background-color: #fff;
             border-bottom: 1px solid #e5e5e5;
+            .logo{
+                width: 241px;
+                height: 50px;
+                line-height: 50px;
+            }
+            .breadcrumb{
+                height: 50px;
+                line-height: 50px;
+                width: calc(100% - 261px);
+                margin-left: 20px;
+                .el-breadcrumb{
+                    height: 50px;
+                    line-height: 50px;
+                }
+            }
         }
         .section{
             height: calc(100% - 51px);
